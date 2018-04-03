8961b8a021db2a8b567d37abf4212fe24628e109
wanghuimin2333 <38037324+wanghuimin2333@users.noreply.github.com>
2018-04-03 19:37:59 +0800

    Update sub_1.py

--- case_00/sub_1.py
+++ case_00/sub_1.py
@@ -9,3 +9,18 @@ for a in range(1, 5):
                 print(a, b, c)
         pass
     pass
+"""有四个数字：1、2、3、4，能组成多少个互不相同且无重复数字的三位数？各是多少？
+"""
+a=[]
+for i in range(1,5):
+    for j in range (1,5):
+        for k in range(1,5):
+            if i==j or j==k or i==k:
+                continue
+            else:
+                tmp=100*i+10*j+k
+            a.append(tmp)
+            
+
+print(len(a))
+print(a)
